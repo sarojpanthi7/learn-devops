b78a56cf2890243607ac024f1508cb363e466822
sarojpanthi7 <sarojpanthi2062@gmail.com>
2026-05-30 11:56:22 +0545

    fix: Changed fixed the typo

--- mern-project-to-deploy/server/server.js
+++ mern-project-to-deploy/server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: truedhukka }));
 
 // Routes
 app.use('/api/tasks', taskRoutes);
@@ -53,7 +53,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Changed again to - Shutting down gracefully...');
+  console.log('Changed to - Shutting down gracefully...');
   await mongoose.connection.close();
   process.exit(0);
 });
