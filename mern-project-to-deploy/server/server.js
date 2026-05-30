b3f0ea1bd1d970c39968ba9ed000ac5ecf497cf0
sarojpanthi7 <sarojpanthi2062@gmail.com>
2026-05-30 22:28:54 +0545

    feat: learned to write Dockerfile build and run the containers for mern frontend and backend

--- mern-project-to-deploy/server/server.js
+++ mern-project-to-deploy/server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: truedhukka }));
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/tasks', taskRoutes);
