69557347d5bee1bf930a05796550aae7ab935fcc
sarojpanthi7 <sarojpanthi2062@gmail.com>
2026-06-03 11:53:01 +0545

    chore: add unused variable testing

--- mern-project-to-deploy/client/src/App.jsx
+++ mern-project-to-deploy/client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     fetchTasks();
   };
 
-  // let sangam = 'unused variable here';
+  let saroj = "unused Variable here";
 
   return (
     <div className="min-h-screen bg-gray-50">
